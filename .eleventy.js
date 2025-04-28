72f55f0b7e426bc8e162e23d5b84312888e997ea
Janne Tielien <janne@tielinen.pro>
2025-04-28 07:26:56 +0300

    hero calendar anchor to /meet

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,6 @@
 module.exports = function (eleventyConfig) {
-  // Pass through the CSS file to the _site directory
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("snippets");
   eleventyConfig.addPassthroughCopy("settings.js");
   eleventyConfig.addPassthroughCopy("_redirects");
-
-  // Optional: You can also pass through other assets like images or JavaScript
-  // eleventyConfig.addPassthroughCopy("images");
-  // eleventyConfig.addPassthroughCopy("scripts");
-
-  // Return your Eleventy configuration object (optional, if you have other settings)
-  return {
-    // Other configurations can go here
-  };
 };
